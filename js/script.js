28c8b4bdba54607f5ee8de1fbfd9eb01a2081218
Dennis Danso - Nightfire Studios <kumidendanso@gmail.com>
2024-09-27 01:01:19 +0000

    eric's click to change icon feature complete

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //All Variable Declarations
 const add_button = document.querySelector('.add_button')
 const task_container = document.querySelector('.task_container')
+const nav_buttons = document.querySelectorAll('.nav_buttons')
 
 add_button.onclick= ()=>{
     task_container.innerHTML =''
@@ -9,3 +10,16 @@ add_button.onclick= ()=>{
         border-bottom: 1px solid gray;
     `
 }
+
+nav_buttons.forEach((items,index)=>{
+
+    items.onclick = ()=>{
+        
+
+        nav_buttons.forEach(buttons=>{
+            buttons.style.width = '30px'
+        })
+
+        items.style.width = '60px'
+    }
+})
